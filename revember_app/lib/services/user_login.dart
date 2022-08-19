85b85cd2e39e1b83f32eb25894a14e24ca7afa5b
Yash <garg.yashvardhan@gmail.com>
2022-08-20 00:36:37 +0400

    fixed the stupid login function

---
+++ revember_app/lib/services/user_login.dart
@@ -0,0 +1,25 @@
+import 'package:revember_app/constants.dart';
+
+Future loginUser(String username, String password) async {}
+
+// Future<bool> checkIfUserExists(String username) async {
+//   var query = await firestore
+//       .collection('users')
+//       .where('username', isEqualTo: username)
+//       .get();
+//   return query.docs.isNotEmpty;
+// }
+
+Future checkPassword(String username, String password) async {
+  var user =
+      firestore.collection('users').where('username', isEqualTo: username);
+  var query = await user.get();
+  if (query.docs.isNotEmpty) {
+    var user = query.docs.first.data();
+    if (user['password'] == password) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
