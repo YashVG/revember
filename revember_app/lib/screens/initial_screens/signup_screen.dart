d062b8a4991e5c954ad47f9e50c967726cfb513f
Yash <garg.yashvardhan@gmail.com>
2023-02-06 20:19:15 +0400

    fixed sign up page completely

--- revember_app/lib/screens/initial_screens/signup_screen.dart
+++ revember_app/lib/screens/initial_screens/signup_screen.dart
@@ -5,6 +5,7 @@ import 'package:revember_app/screens/initial_screens/signup_screen2.dart';
 
 import 'package:revember_app/services/user_services/user_creation.dart';
 import 'package:revember_app/components/back_button.dart';
+import 'package:revember_app/services/user_services/sign_up_checker.dart';
 
 class SignUpScreen extends StatefulWidget {
   static const String id = 'signup_screen';
@@ -58,7 +59,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ElevatedButton(
                 child: Text('Continue'),
                 onPressed: () async {
-                  if (username == null || email == null) {
+                  //checks if user has inputted anything in field
+                  if (username == '' || email == '') {
                     return showDialog(
                       context: context,
                       builder: (context) {
@@ -77,6 +79,47 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       },
                     );
                   }
+                  //checks if username is up to conventional standards
+                  if (usernameChecker(username) == false) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Username too simple'),
+                          content: Text('Please change your username'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Okay'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                  //checks if email inputted has a proper format and address
+                  if (emailChecker(email) == false) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Email invalid'),
+                          content: Text('Please input a valid email address'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Okay'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                  //checks if username already exists with asynchronous function
                   if (await checkDuplicateUsername(username) == true) {
                     showDialog(
                       context: context,
